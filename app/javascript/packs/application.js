a87a2b1888f8ff63b1606b0db6931c2fc786e615
GANESH-DESHMUK <dganesh@vardhaman.org>
2021-11-30 20:55:10 +0530

    configure/application.erb update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "chartkick";
-import "chart.js";
+import "chart.js"
 
 Rails.start()
 Turbolinks.start()
